0d3cd7a81b7b3cae3e1def3a8cda745ebf1d4809
jagperez84 <jag.perez.84@gmail.com>
2026-08-15 00:33:18 +0200

    style: apply global field sizing standard

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import { AuthProvider } from './auth/AuthContext';
 import './styles/theme.css';
+import './styles/field-sizing.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><BrowserRouter><AuthProvider><App /></AuthProvider></BrowserRouter></React.StrictMode>
