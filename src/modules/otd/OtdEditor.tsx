68c5203a9d521dc945738417f85c1cac514cca96
jagperez84 <jag.perez.84@gmail.com>
2026-08-21 16:00:30 +0200

    fix(otd): apply editor picker and formulation UI fixes

--- src/modules/otd/OtdEditor.tsx
+++ src/modules/otd/OtdEditor.tsx
@@ -1,1 +1,9 @@
-export { OtdEditorV2 as OtdEditor, OtdList } from './OtdEditorV2';
+import React from 'react';
+import { OtdEditorV2, OtdList } from './OtdEditorV2';
+import './otd-editor-fixes.css';
+
+export function OtdEditor() {
+  return <OtdEditorV2 />;
+}
+
+export { OtdList };
