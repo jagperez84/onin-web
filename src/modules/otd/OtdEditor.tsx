5c666e18622b43b664400d612f911015894a35a5
jagperez84 <jag.perez.84@gmail.com>
2026-08-22 12:28:53 +0200

    fix(otd): apply picker and autocomplete UI fixes

--- src/modules/otd/OtdEditor.tsx
+++ src/modules/otd/OtdEditor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { OtdEditorV2, OtdList } from './OtdEditorV2';
 import './otd-editor-fixes.css';
+import './otd-editor-ui-fixes.css';
 
 export function OtdEditor() {
   return <OtdEditorV2 />;
